6dad19dbb73361e6beaddbab429c3ffa09143f9f
benjamin.xu <benjamin.xu@wegames.biz>
2019-11-14 11:32:00 +0800

    触发检测系统

---
+++ FootStone.TankShared/Behaviour/TriggerDataBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Assets.Scripts.ECS
+{
+	public class TriggerDataBehaviour : MonoBehaviour, IConvertGameObjectToEntity
+	{
+		public TriggerVolumeType type = TriggerVolumeType.None;
+		void IConvertGameObjectToEntity.Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+		{
+			var com = new TriggerDataComponent
+			{
+				VolumeType = (int)type
+			};
+			dstManager.AddComponentData(entity, com);
+		}
+	}
+}
