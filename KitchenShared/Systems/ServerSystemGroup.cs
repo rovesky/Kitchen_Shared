7462fb995bae1e9fed6b71fb87c784a5e06edf84
sam.lu <lsj@com>
2020-05-08 18:30:56 +0800

    重构代码

--- KitchenShared/Systems/ServerSystemGroup.cs
+++ KitchenShared/Systems/ServerSystemGroup.cs
@@ -10,13 +10,15 @@ namespace FootStone.Kitchen
         {
           //  m_systemsToUpdate.Add(World.GetOrCreateSystem<CharacterPickupBoxSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<FoodSlicedSystem>());
-            m_systemsToUpdate.Add(World.GetOrCreateSystem<CountdownSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<ServeSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<PlateServedSystem>());
-            m_systemsToUpdate.Add(World.GetOrCreateSystem<MenuSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<PlateRecycleSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<UpdatePlateProductSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<CharacterDishOutSystem>());
+
+
+            m_systemsToUpdate.Add(World.GetOrCreateSystem<CountdownSystem>());
+            m_systemsToUpdate.Add(World.GetOrCreateSystem<MenuSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<GameStartSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<GameEndSystem>());
             m_systemsToUpdate.Add(World.GetOrCreateSystem<GamePrepareSystem>());
