46938e6c3ff31f4688f8e1a0f23dc92cb8314b3f
sam.lu <lsj@com>
2020-05-29 22:13:24 +0800

    拾取被投掷的食物

--- KitchenShared/Character/Pickup/CharacterPickupFlyingSystem.cs
+++ KitchenShared/Character/Pickup/CharacterPickupFlyingSystem.cs
@@ -1,5 +1,7 @@
 ﻿using FootStone.ECS;
 using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
 
 namespace FootStone.Kitchen
 {
@@ -8,42 +10,63 @@ namespace FootStone.Kitchen
     {
         protected override void OnUpdate()
         {
+            //开始接飞来的食物
             Entities
                 .WithAll<ServerEntity>()
+                .WithName("CharacterPickupFlyingBegin")
                 .WithStructuralChanges()
                 .ForEach((Entity entity,
-                ref TriggerPredictedState triggerState,
-                in SlotPredictedState slotState,
-                in UserCommand command) =>
-              {
+                    ref TriggerPredictedState triggerState,
+                    ref CharacterMovePredictedState movePredictData,
+                    ref TransformPredictedState transformState,
+                    in CharacterMoveSetting moveSetting,
+                    in SlotPredictedState slotState,
+                    in UserCommand command) =>
+                {
+                    //  FSLog.Info($"CharacterPickupFlyingSystem,entity:{entity}");
+                    //if (command.Buttons.IsSet(UserCommand.Button.Button2))
+                    //   return;
 
-                //  FSLog.Info($"CharacterPickupFlyingSystem,entity:{entity}");
-                if (command.Buttons.IsSet(UserCommand.Button.Button2))
-                   return;
+                    var pickupEntity = slotState.FilledIn;
 
-                var pickupEntity = slotState.FilledIn;
-               
-                if (pickupEntity != Entity.Null || triggerState.TriggeredEntity == Entity.Null) 
-                    return;;
+                    if (pickupEntity != Entity.Null || triggerState.TriggeredEntity == Entity.Null)
+                        return;
 
-                if(!EntityManager.HasComponent<Item>(triggerState.TriggeredEntity))
-                    return;
-                   
-                var item = EntityManager.GetComponentData<OwnerPredictedState>(triggerState.TriggeredEntity);
-              //  FSLog.Info($"PickUpItem flying,PreOwner:{item.PreOwner},entity:{entity}");
-                if(item.PreOwner == Entity.Null ||  item.PreOwner == entity)
-                    return;
+                    if (!HasComponent<Flying>(triggerState.TriggeredEntity))
+                        return;
 
-                var worldTick = GetSingleton<WorldTime>().Tick;
-                FSLog.Info($"PickUpItem flying,command tick:{command.RenderTick},worldTick:{worldTick}");
- 
-                ItemAttachUtilities.ItemAttachToOwner(EntityManager,
-                    triggerState.TriggeredEntity,entity,Entity.Null);
-                  
-            //    pickupState.PickupedEntity = triggerState.TriggeredEntity;
-                triggerState.TriggeredEntity = Entity.Null;
+                    if (!HasComponent<Item>(triggerState.TriggeredEntity))
+                        return;
 
-            }).Run();
+                    var item = EntityManager.GetComponentData<OwnerPredictedState>(triggerState.TriggeredEntity);
+                    //  FSLog.Info($"PickUpItem flying,PreOwner:{item.PreOwner},entity:{entity}");
+                    if (item.PreOwner == Entity.Null || item.PreOwner == entity)
+                        return;
+
+                    var worldTick = GetSingleton<WorldTime>().Tick;
+                    FSLog.Info($"PickUpItem flying,command tick:{command.RenderTick},worldTick:{worldTick}");
+
+                    ItemAttachUtilities.ItemAttachToOwner(EntityManager,
+                     triggerState.TriggeredEntity,entity,Entity.Null);
+                    triggerState.TriggeredEntity = Entity.Null;
+                    /*
+                    var itemTransform = GetComponent<TransformPredictedState>(triggerState.TriggeredEntity);
+                    var velocity = math.normalize(itemTransform.Position - transformState.Position) * 8.0f;
+                    velocity.y = 0;
+                    movePredictData.ImpulseVelocity = velocity;
+                    movePredictData.ImpulseDuration = 5;
+
+                    var fromRotation = transformState.Rotation;
+                    var toRotation = quaternion.LookRotationSafe(velocity, math.up());
+                    var angle = Quaternion.Angle(fromRotation, toRotation);
+                    transformState.Rotation = Quaternion.RotateTowards(fromRotation, toRotation,
+                        math.abs(angle - 180.0f) < float.Epsilon
+                            ? -moveSetting.RotationVelocity
+                            : moveSetting.RotationVelocity);*/
+
+                
+
+                }).Run();
         }
     }
 }
